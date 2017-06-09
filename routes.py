10aa0cccb582a749fb7ab0cbb2df217e4acf9a3c
gffrancheska <gffrancheska@gmail.com>
2017-06-08 21:13:47 -0400

    Adding comments to the code.

--- routes.py
+++ routes.py
@@ -1,13 +1,19 @@
+# import the Flask class, and the function render_template
 from flask import Flask, render_template
 
+# create a new instance of the Flask class and save it into the variable app
 app = Flask(__name__)
 
+# map the url "/" to the function "index"
 @app.route("/")
 def index():
+  # return the index.html template
   return render_template("index.html")
 
+# map the url "/about" to the function "about"
 @app.route("/about")
 def about():
+  # return the about.html template
   return render_template("about.html")
 
 if __name__ == "__main__":
